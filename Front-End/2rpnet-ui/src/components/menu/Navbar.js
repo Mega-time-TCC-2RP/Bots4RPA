daf6c97a0214a46399faceecc9ba4843be67edb6
Marcos Paulo Do Prado Martiro Filho <51371754802@info.scs>
2022-03-28 16:55:48 -0300

    Problema do menu lateral resolvido

--- Front-End/2rpnet-ui/src/components/menu/Navbar.js
+++ Front-End/2rpnet-ui/src/components/menu/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../assets/css/components/navbar.css'
 
+
 // Icons from react-icons
 import * as FaIcons from 'react-icons/fa'
 import * as ImIcons from 'react-icons/im'
@@ -15,65 +16,68 @@ import Logo from '../../assets/img/logo2RP.png'
 import Profile from '../../assets/img/profile.jpg'
 
  function Navbar() {
-  let btn = document.querySelector('#btn')
-  let sidebar = document.querySelector('.sidebar')
-
-  btn.onclick = function(){
-    sidebar.classList.toggle("active")
+  
+  function click(){
+    let sidebar = document.querySelector('.sidebar')
+    sidebar.classList.toggle('active')
   }
+  
+  // btn.onclick = function(){
+  //   sidebar.classList.toggle('active')
+  // }
+  // onClick={sidebar.classList.toggle('active')}
+return (
 
-  return (
-<div id='global'>
     <div className='sidebar'>
       <div className='logo_content'>
         <img className='logo' src={Logo} alt="Logo 2RPnet"/>
-        <FaIcons.FaBars id="btn"/>
+        <FaIcons.FaBars className='btn' onClick={click} />
       </div>
       <ul className='nav_list'>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <ImIcons.ImHome3 className='icon'/>
             <span className='Links_name'>Home</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <RiIcons.RiGuideFill className='icon'/>
             <span className='Links_name'>Guias</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <FaIcons.FaUniversalAccess className='icon'/>
             <span className='Links_name'>Acessibilidade</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <FaIcons.FaTasks className='icon'/>
             <span className='Links_name'>Tarefas</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <FaIcons.FaTshirt className='icon'/>
             <span className='Links_name'>Coleção</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <FaIcons.FaRobot className='icon'/>
             <span className='Links_name'>Processos</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <AiIcons.AiFillMessage className='icon'/>
             <span className='Links_name'>Social</span>
           </Link>
         </li>
         <li>
-          <Link to="#" className=' Link'>
+          <Link to="#" className='Link'>
             <BsIcons.BsFillGearFill className='icon'/>
             <span className='Links_name'>Configurações</span>
           </Link>
@@ -86,16 +90,18 @@ import Profile from '../../assets/img/profile.jpg'
             <img src={Profile} alt="img profile"/>
               <div className='name_job'>
                 <div className='name'>Name</div>
-                <dib className='job'>Job</dib>
+                <div className='job'>Job</div>
               </div>
             </div>
             <HiIcons.HiOutlineLogout id='log_out'/>
           </div>
         </div>
      </div>
-     <div className='home_content'></div>
-</div>
+     
+
   );
+ 
+
 }
 export default Navbar;
 
