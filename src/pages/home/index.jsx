11bafb015efeb5caa011c24ff3f9170a9d08672d
Lucca Gomes Barreto <45923499894@info.scs>
2022-04-01 17:19:56 -0300

    começando home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,21 @@
 import "../../assets/css/style.css"
 import Navbar from '../../components/menu/Navbar'
+{/* <Navbar/> */}
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
-      <h1 className="test">Oi</h1>
-  </div>
-
+      <div className="top-container">
+        <div className="top-buttons">
+          <form>
+          <button className="button-assistant">Criar Assistente</button>
+          <input></input>
+          <span className="search-assistant">Buscar Assistentes</span>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
