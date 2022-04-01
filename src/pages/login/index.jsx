c0c6839be4a698101ca0f9937e8cc16b75afc74e
Lucca Gomes Barreto <45923499894@info.scs>
2022-04-01 16:48:47 -0300

    iniciando parte lógica

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios, { Axios } from 'axios';
 import { Link } from 'react-router-dom';
 
@@ -21,7 +21,17 @@ import '../../assets/css/components/button.css';
 import '../../assets/css/components/fonts.css';
 
 
-export default function login() {
+export default function Login() {
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+
+        const handleSubmit = (e) => {
+                e.preventDefault();
+
+                console.log("LOGADO", {email, password});
+        };
+
+
         return (
                 <div>
                         <div className='login'>
@@ -31,17 +41,30 @@ export default function login() {
                                         
                                         <div className='forms-login'>
                                         <img src={logoMaior} className='logo-Header' alt="Logo 2RP" />
-
+                                        <form onSubmit={handleSubmit}>
                                                 <div className='formsLogin-Email'>
                                                         <p className='topo-input-email'>E-mail</p>
-                                                        <input type="email" placeholder='Insira seu E-mail' />
+                                                        <input
+                                                                type="email"
+                                                                placeholder='Insira seu E-mail'
+                                                                id="email"
+                                                                value={email}
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                        />
                                                 </div>
                                                 <div className='Login'>
                                                         <p className='topo-input-senha'>Senha</p>
-                                                        <input type="senha" placeholder='Insira sua senha' />
+                                                        <input
+                                                                type="senha"   
+                                                                placeholder='Insira sua senha'
+                                                                id="senha"
+                                                                value={password}
+                                                                onChange={(e) => setPassword(e.target.value)}        
+                                                        />
                                                 </div>
+                                                
                                                 <button className='botaoLogin'>Logar</button>
-
+                                                
                                                 <div className='EsqueceuSenha'>
                                                         <a className='re-passwq'>Esqueceu a senha?</a>
                                                 </div>
@@ -55,6 +78,7 @@ export default function login() {
                                                 <div className='NotSigned-login'>
                                                         <a>Não possui cadastro?</a>
                                                 </div>
+                                                </form>
                                                 <div>
                                                         
                                                 </div>
