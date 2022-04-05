29e9da1795512ba091d2186b6d2adb2a33844222
PedroLCamara <plcrlschool@gmail.com>
2022-04-05 17:22:21 -0300

    ModaisIncompletos

--- Front-End/2rpnet-ui/src/index.js
+++ Front-End/2rpnet-ui/src/index.js
@@ -17,7 +17,7 @@ import Login from './pages/login/';
 import Guide from './pages/guide/';
 import Marketplace from './pages/marketplace/';
 import MyProcesses from './pages/myProcesses/';
-import Social from './pages/social/index';
+import {TelaTimeline} from './pages/social/index';
 import TaskCalendar from './pages/taskCalendar';
 import TaskKanban from './pages/taskKanban';
 
@@ -32,15 +32,15 @@ const routing = (
       <Routes>
         <Route exact path="/" element={<Home/>} /> {/* Home */}
         <Route path="/landingPage" element={<LandingPage/>} />
-        <Route path="/login" element={<Login/>} /> {/* Login */}
-        <Route path="/guide" element={<Guide/>} /> {/* Guide */}
-        <Route path="/marketplace" element={<Marketplace/>} /> {/* Marketplace */}
-        <Route path="/myprocesses" element={<MyProcesses/>} /> {/* MyProcesses */}
-        <Route path="/social" element={<Social/>} /> {/* Social */}
-        <Route path="/taskcalendar" element={<TaskCalendar/>} /> {/* Task Calendar */}
-        <Route path="/taskkanban" element={<TaskKanban/>} /> {/* Task Kanban */}
+        {/* <Route path="/login" element={<Login/>} /> {/* Login */}
+        {/*<Route path="/guide" element={<Guide/>} /> {/* Guide */}
+        {/*<Route path="/marketplace" element={<Marketplace/>} /> {/* Marketplace */}
+        {/*<Route path="/myprocesses" element={<MyProcesses/>} /> {/* MyProcesses */}
+        <Route path="/social" element={<TelaTimeline/>} /> {/* Social */}
+        {/*<Route path="/taskcalendar" element={<TaskCalendar/>} /> {/* Task Calendar */}
+        {/*<Route path="/taskkanban" element={<TaskKanban/>} /> {/* Task Kanban */}
 
-        <Route path="/config" element={<Config/>} /> {/* Configuration */}
+        {/*<Route path="/config" element={<Config/>} /> {/* Configuration */}
      
         <Route path="/notFound" element={<NotFound/>} /> {/* Not Found */}
         <Route path="*" element={<Navigate to="notFound"/>} /> {/*Redireciona para Not Found caso não encontre nenhuma rota*/}
