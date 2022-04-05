dd3830b37b47210a24081c6755c7b89629fd36a6
z-eck <erick_escarlate@outlook.com>
2022-04-05 17:32:05 -0300

    aaaa o professor ta me apressando

--- Front-End/2rpnet-ui/src/pages/taskCalendar/index.jsx
+++ Front-End/2rpnet-ui/src/pages/taskCalendar/index.jsx
@@ -13,93 +13,124 @@ function App() {
   return (
 
     <div className='pageTaskCalendar'>
-      {/* <Header /> */}
-      {/* <Navbar /> */}
+      <Header />
+      <Navbar />
       <h3 className="pageTitle h3">Painel Organizacional</h3>
       <div className='taskCalendar'>
         <section className="task">
           <div className="toDo">
             <h5 className="taskTitle h5">A Fazer</h5>
-            <div className="taskSpace">
+            <div className="taskSpace" draggable="true">
               <p className="cardTask p">Lorem Ipsum is simply dummy text.</p>
             </div>
           </div>
           <div className="do">
             <h5 className="taskTitle h5">Fazendo</h5>  
-            <div className="taskSpace">
+            <div className="taskSpace" draggable="true">
               <p className="cardTask p">Lorem Ipsum is simply dummy text of the printing.</p>
             </div>
           </div>
           <div className="done">
             <h5 className="taskTitle h5">Feito</h5>
-            <div className="taskSpace">
+            <div className="taskSpace" draggable="true">
               <p className="cardTask p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
           </div>
         </section>
         <div className="calendarAndBtn">
           <section className="calendar">
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="1" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="2" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="3" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="4" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="5" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="6" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="7" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="8" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="9" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="10" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="11" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="12" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="13" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="14" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="15" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="16" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="17" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="18" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="19" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="20" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="21" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="22" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="23" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="24" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="25" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="26" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="27" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="28" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="29" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="30" />
-            <input type="button"
+            <input className="btnCalendar"
+            type="button"
             value="31" />
           </section>
           <input
