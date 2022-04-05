559369c3e2759368e456cced839e4976abecb042
Lucca <luccag.barreto@gmail.com>
2022-04-04 21:53:59 -0300

    top e body finalizados, faltando barra e bottom da pagina serem finalizados

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,16 @@
 import "../../assets/css/style.css"
+import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios, { Axios } from 'axios';
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/menu/Navbar'
 
 //img:
 import Azul_Home from '../../assets/img/Azul_Home.png'
+import Vermelho_Home from '../../assets/img/Vermelho_Home.png'
+import Amarelo_Home from '../../assets/img/Amarelo_Home.png'
+import Verde_Home from '../../assets/img/Verde_Home.png'
+import Phost_Perfil_Photo from '../../assets/img/Phost_Perfil_Photo.png'
 
 
 //Components:
@@ -21,7 +29,7 @@ function App() {
           <button className="button-assistant">Criar Assistente</button>
           <input type='search' placeholder="Buscar assistente" id="Assistente"></input>
           </form>
-          <form>
+          <form className="nao">
             <div className="card1">
               <img src={Azul_Home} className="card1-img"/>
               <h5>Assistente 1</h5>
@@ -29,20 +37,20 @@ function App() {
               <a className="details-button">Ver detalhes</a>
             </div>
             <div className="card1">
-              <img src={Azul_Home} className="card1-img"/>
-              <h5>Assistente 1</h5>
+              <img src={Vermelho_Home} className="card1-img"/>
+              <h5>Assistente 2</h5>
               <a className="play-button">▶</a>
               <a className="details-button">Ver detalhes</a>
             </div>
             <div className="card1">
-              <img src={Azul_Home} className="card1-img"/>
-              <h5>Assistente 1</h5>
+              <img src={Amarelo_Home} className="card1-img"/>
+              <h5>Assistente 3</h5>
               <a className="play-button">▶</a>
               <a className="details-button">Ver detalhes</a>
             </div>
             <div className="card1">
-              <img src={Azul_Home} className="card1-img"/>
-              <h5>Assistente 1</h5>
+              <img src={Verde_Home} className="card1-img"/>
+              <h5>Assistente 4</h5>
               <a className="play-button">▶</a>
               <a className="details-button">Ver detalhes</a>
             </div>
@@ -68,8 +76,22 @@ function App() {
               <p className="data-body">Data de entrega : xx/xx/xxxx</p>
             </div>
         </form>
-
-
+      </div>
+      <div className="bottom-container">
+        <form>
+          <div className="forms-items">
+            <h2 className="bottom-title">Posts em destaque</h2>
+            <div className="bottom-posts-content">
+              <img src={Phost_Perfil_Photo} className="ItemPost-avatar"/>
+              <div className="chatItemList-line">
+                <div className="PostItem-name">Marcos</div>
+              </div>
+              <div className="chatItemList-line">
+                <div className="PostItem-role">DevOps</div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
