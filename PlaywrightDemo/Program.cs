ee491825fa45602ee5296199b1087217dbc756a0
ricardo-rtc <ricardotamahecarvalho@gmail.com>
2022-04-20 11:14:13 -0300

    Add Playwright Test

---
+++ PlaywrightDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true,
+            SlowMo = 1000
+        });
+        var context = await browser.NewContextAsync();
+
+        // Open new page
+        var page = await context.NewPageAsync();
+
+        // Go to https://www.google.com/?gws_rd=ssl
+        await page.GotoAsync("https://www.google.com/?gws_rd=ssl");
+
+        await page.FillAsync("input[title='Pesquisar']", "valor dolar");
+        
+        // Hit Enter
+        await page.PressAsync("input[title='Pesquisar']", "Enter");
+
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+        
+
+    }
+}
