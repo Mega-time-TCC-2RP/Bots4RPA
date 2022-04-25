cb7cc31a1081919dfb5b0b2c5911ecba3ff8fec3
z-eck <erick_escarlate@outlook.com>
2022-04-25 17:22:09 -0300

    dia 4 dos testes no back-end

--- Rpa.Test/Controllers/CommentsControllerTest.cs
+++ Rpa.Test/Controllers/CommentsControllerTest.cs
@@ -18,35 +18,49 @@ namespace Rpa.Test.Controllers
         public void Must_Return_Comment_Created()
         {
             // Arrange
-            var mockRepo = new Mock<ICommentRepository>();
-            mockRepo
-                .Setup(x => x.Create()))
-                .Returns((Comment)null);
+            //var mockRepo = new Mock<ICommentRepository>();
+            //mockRepo
+            //    .Setup(x => x.Create()))
+            //    .Returns((Comment)null);
 
-            var fakeDomain = new Comment();
-            fakeDomain.IdPost = 1;
-            fakeDomain.Title = "teste@gmail.com";
-            fakeDomain.CommentDescription = "TesteT3ste";
-            fakeDomain.DataComment = DateTime.Now;
+            //var fakeDomain = new Comment();
+            //fakeDomain.IdPost = 1;
+            //fakeDomain.Title = "teste@gmail.com";
+            //fakeDomain.CommentDescription = "TesteT3ste";
+            //fakeDomain.DataComment = DateTime.Now;
 
-            var controller = new CommentsController(mockRepo.Object);
+            //var controller = new CommentsController(mockRepo.Object);
+            throw new NotImplementedException();
         }
 
         [Fact]
         public void Must_Return_Comment_Read()
         {
-            // Arrange
-            var mockRepo = new Mock<ICommentRepository>();
-            mockRepo
-                .Setup(x => x.ReadAll());
 
-            var controller = new CommentsController();
+            var mockRepo = new Mock<ICommentRepository>();
+            var mockEmployee = new Mock<IEmployeeRepository>();
+            var mockUser = new Mock<IUserNameRepository>();
+            var mockPlayer = new Mock<IPlayerRepository>();
+            mockRepo.Setup(x => x.ReadAll());
+            var controller = new CommentsController((ICommentRepository)mockRepo, (IEmployeeRepository)mockEmployee, (IUserNameRepository)mockUser, (IPlayerRepository)mockPlayer);
+
+            var result = controller.ReadAll();
+
+            Assert.IsType<OkObjectResult>(result);
+
+            // Arrange
+            //var mockRepo = new Mock<ICommentRepository>();
+            //mockRepo
+            //    .Setup(x => x.ReadAll())
+            //    .Returns(new List<Comment>());
+
+            //var controller = new CommentsController(mockRepo.Object, mockRepo.Setups, mockRepo.Switches, mockRepo.Invocations);
 
             // Act
-            var result = controller.ControllerContext();
+            //var result = controller.ControllerContext();
 
             // Assert
-            Assert.IsType<OkObjectResult>(result);
+            //Assert.IsType<OkObjectResult>(result);
         }
 
         [Fact]
---
+++ Rpa.Test/Controllers/UserTypesControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using _2rpnet.rpa.webAPI.Contexts;
+using _2rpnet.rpa.webAPI.Controllers;
+using _2rpnet.rpa.webAPI.Domains;
+using _2rpnet.rpa.webAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rpa.Test.Controllers
+{
+    public class UserTypesControllerTest : ControllerBase
+    {
+
+        [Fact]
+        public void Must_Return_Ok_in_UserType_Read()
+        {
+            // Arrange
+            var mockRepo = new Mock<IUserTypeRepository>();
+            mockRepo.Setup(x => x.ReadAll());
+            var controller = new UserTypesController(mockRepo.Object);
+
+            // Act  
+            var result = controller.ReadAll();
+
+            // Assert  
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Must_Return_NotFound_in_UserType_Read()
+        {
+            // Arrange
+            var mockRepo = new Mock<IUserTypeRepository>();
+            mockRepo.Setup(x => x.ReadAll());
+            var controller = new UserTypesController(mockRepo.Object);
+
+            // Act  
+            var result = controller.ReadAll();
+
+            // Assert  
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Must_Return_Ok_in_UserType_Searched_For_By_ID()
+        {
+            // Arrange
+            var mockRepo = new Mock<IUserTypeRepository>();
+            //var mockID = 1;
+            //mockRepo.Setup(x => x.SearchByID(1));
+            var controller = new UserTypesController(mockRepo.Object);
+
+            // Act
+            var result = controller.SearchByID(2);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
