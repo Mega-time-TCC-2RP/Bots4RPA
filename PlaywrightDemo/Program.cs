552764899999d53370db03d287231870b0f0e086
Gustavo Borges de Souza <gustavo.souza14@portalsesisp.org.br>
2022-05-04 11:20:50 -0300

    Procedimentos de playright para noticias

--- PlaywrightDemo/Program.cs
+++ PlaywrightDemo/Program.cs
@@ -10,7 +10,7 @@ class Program
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false,
-            SlowMo = 3000
+            SlowMo = 4000
         });
         var context = await browser.NewContextAsync();
 
@@ -20,23 +20,43 @@ class Program
         // Go to https://www.google.com/?gws_rd=ssl
         await page.GotoAsync("https://www.google.com/?gws_rd=ssl");
 
-        await page.FillAsync("input[title='Pesquisar']", "dilica da nona");
+        await page.FillAsync("input[title='Pesquisar']", "chocolaate");
         
         // Hit Enter
         await page.PressAsync("input[title='Pesquisar']", "Enter");
 
-        //entra no link que aparece quando pesquisa
+        //entra no 1° link que aparece quando pesquisa
         // await page.ClickAsync("h3.LC20lb.MBeuO.DKV0Md");
+        // await page.ClickAsync("h3.mCBkyc.y355M.JQe2Ld.nDgy9d");
+
+        ////////////////////////////////////////////////////////
 
         //entra na parte de imagens do google e pega primeira imagem
-        await page.Locator("#hdtb-msb >> text=Imagens").ClickAsync();
+        // await page.Locator("#hdtb-msb >> text=Imagens").ClickAsync();
 
-        await page.ClickAsync("img.rg_i.Q4LuWd");
+        // await page.ClickAsync("img.rg_i.Q4LuWd");
 
         //clica na primeira imagem e vai pro link dela
-        await page.ClickAsync("img.n3VNCb");
+        // await page.ClickAsync("span.KSvtLc");
+        // await page.ClickAsync("img.n3VNCb");
 
-        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+        //////////////////////////////////////////////////////
+
+        await page.Locator("#hdtb-msb >> text=Notícias").ClickAsync();
+
+        await page.ClickAsync("div.mCBkyc.y355M.JQe2Ld.nDgy9d");
+
+
+        const {browser}=this.helpers.Playwright;
+        await browser.pages(); //list pages in the browser
+
+        //get current page
+        const {page}=this.helpers.Playwright;
+        const url=await page.url();//get the url of the current page
+        Console.WriteLine(url);
+
+
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path =  "screenshot.png"});
 
     }
 }
