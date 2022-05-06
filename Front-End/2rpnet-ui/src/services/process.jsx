56618ef78bbf738e43b0a40cf14c0eda1c8dd1ba
Gustavo Borges de Souza <gustavo.souza14@portalsesisp.org.br>
2022-05-06 08:00:23 -0300

    procedimentos alterados

--- Front-End/2rpnet-ui/src/services/process.jsx
+++ Front-End/2rpnet-ui/src/services/process.jsx
@@ -3,55 +3,53 @@ export default
         {
             "IdProcedure": 1,
             "ProcedurePriority": null,
-            "ProcedureName": "Numero Inicial",
-            "ProcedureDescription": "Coloca um número inicial",
-            "ProcedureValue": 0,
-            "ProcedureType": "math",
-            "ProcedureFunction": "DefineIValue()"
+            "ProcedureName": "Pesquisar no google",
+            "ProcedureDescription": "Pesquisa o que você quiser no google",
+            "ProcedureValue": "",
         },
         {
             "IdProcedure": 2,
             "ProcedurePriority": null,
-            "ProcedureName": "Somar",
-            "ProcedureDescription": "Soma um numero com o  valor desse",
-            "ProcedureValue": 0,
-            "ProcedureType": "math",
-            "ProcedureFunction": "Sum()"
+            "ProcedureName": "Entrar no primeiro link",
+            "ProcedureDescription": "Entra no primeiro link do que foi pesquisado",
+            "ProcedureValue": "",
         },
         {
             "IdProcedure": 3,
             "ProcedurePriority": null,
-            "ProcedureName": "Subtrair",
-            "ProcedureDescription": "Subtrai um numero com o valor desse",
-            "ProcedureValue": 0,
-            "ProcedureType": "math",
-            "ProcedureFunction": "Subtract()"
+            "ProcedureName": "Ir para uma URL",
+            "ProcedureDescription": "Vai para a url que é o valor desse",
+            "ProcedureValue": "",
         },
         {
             "IdProcedure": 4,
             "ProcedurePriority": null,
-            "ProcedureName": "Multiplicar",
-            "ProcedureDescription": "Multiplica um numero com o valor desse",
-            "ProcedureValue": 0,
-            "ProcedureType": "math",
-            "ProcedureFunction": "Multiply()"
+            "ProcedureName": "Entrar na guia imagens",
+            "ProcedureDescription": "Entra na parte de imagens do que foi pesquisado",
+            "ProcedureValue": "",
         },
-        // {
-        //     "IdProcedure": 5,
-        //     "ProcedurePriority": null,
-        //     "ProcedureName": "Ler email",
-        //     "ProcedureDescription": "Lê o último e mail e busca informações",
-        //     "ProcedureValue": "",
-        //     "ProcedureType": "email"
-        // },
-        // {
-        //     "IdProcedure": 6,
-        //     "ProcedurePriority": null,
-        //     "ProcedureName": "Mandar email",
-        //     "ProcedureDescription": "Lê o último e mail e busca informações",
-        //     "ProcedureValue": "",
-        //     "ProcedureType": "email"
-        // },
+        {
+            "IdProcedure": 5,
+            "ProcedurePriority": null,
+            "ProcedureName": "Clicar na primeira imagem",
+            "ProcedureDescription": "Multiplica um numero com o valor desse",
+            "ProcedureValue": "",
+
+        },
+        {
+            "IdProcedure": 6,
+            "ProcedurePriority": null,
+            "ProcedureName": "Entrar na guia noticias",
+            "ProcedureDescription": "Entra na parte de noticias do que foi pesquisado",
+            "ProcedureValue": "",
+        },
+        {
+            "IdProcedure": 7,
+            "ProcedurePriority": null,
+            "ProcedureName": "Clicar na primeira noticia",
+            "ProcedureDescription": "Entra na primeira notícia",
+            "ProcedureValue": "",
+        },
     ]
 
 
