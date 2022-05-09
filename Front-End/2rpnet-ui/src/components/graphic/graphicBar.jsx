6a4d4afce9826dcc4db4055d3907169abb7a3af4
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2022-05-08 23:16:34 -0300

    tentando arrumar gráfico

--- Front-End/2rpnet-ui/src/components/graphic/graphicBar.jsx
+++ Front-End/2rpnet-ui/src/components/graphic/graphicBar.jsx
@@ -3,60 +3,55 @@ import BarChart from '../../components/graphic/rendergraphic'
 import { getUserData } from './dataBar'
 // import { UserData } from './dataBar'
 
-export default function BarGraphic() {
-    // ------------------------------------------//
+// export default function BarGraphic() {
+// ------------------------------------------//
 
-    var List = []; 
 
-    function getUserData() {
-        fetch('https://grupo8api.azurewebsites.net/api/Run/ListAll', {
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('2rp-chave-autenticacao'),
-            },
-        })
-            .then((response) =>{
-                console.log("antes");
-                console.log(response.json());
-                response.json()
+export default function GraphicBar() {
+
+    const [chart, setChart] = useState([])
+
+    var baseUrl = "https://grupo8api.azurewebsites.net/api/Run/ListAll"
+    var header = {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('2rp-chave-autenticacao'),
+        },
+    }
+    useEffect(() => {
+        const fetchRun = async () => {
+            fetch(baseUrl, header
+            ).then((response) => {
+                response.json().then((json) => {
+                    console.log(json)
+                    setChart(json.data)
+                })
+            }).catch(error => {
+                console.log(error);
             })
-            .catch((error) => console.log(error));
-
-    }
-    
-    useEffect(setUserData(getUserData()), [])
-
-    let userData = [];
-
-    function setUserData(list){
-        console.log(list);
-
-        userData =
-        {
-            labels: list.map((data) => console.log(data)),
-    
-            datasets: [
-                {
-                    label: "Qtde",
-                    data: "2",
-                    // data: "33",
-                    backgroundColor: [
-                        "#3FDA9F",
-                    ],
-                },
-            ],
         }
-    }
-
+       
+        fetchRun()
+    }, [])
    
-
-
+    const data = {
+        // labels: list.map((data) => console.log(data)),
+        //  labels: chart..map(x => x.runDate),
+         labels: ['oia'],
+        datasets: [
+            {
+                label: "Qtde",
+                // data: [chart.ListAll.map(x => x.runQuantity)],
+                data: [12],
+                backgroundColor: [
+                    "#3FDA9F",
+                ],
+            },
+        ],
+    }
     return (
         <div>
             <div className="container_grafico">
-                {
-                     console.log(List)
-                }
-                <BarChart chartData={userData} className="" />
+                <BarChart chartData={data} className="" />
             </div>
         </div>
     )
