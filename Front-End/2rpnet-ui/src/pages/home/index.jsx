7a9c24e86b8da7de3297c76b8ae45601f0158c72
GustavoBorgesSouza <sgustavo.borges10@gmail.com>
2022-05-22 11:12:28 -0300

    descomentando coisas manha

--- Front-End/2rpnet-ui/src/pages/home/index.jsx
+++ Front-End/2rpnet-ui/src/pages/home/index.jsx
@@ -215,7 +215,7 @@ export default function Home() {
     console.log(parseJwt())
     GetMyQuests();
     GetHighlightedPosts();
-    // GetMyAssistants();
+    GetMyAssistants();
   }, [])
 
   return (
