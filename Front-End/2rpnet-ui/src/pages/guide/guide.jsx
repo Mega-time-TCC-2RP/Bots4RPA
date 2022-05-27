e5343e364c4aa2eb145b44d474bd69ef377c176f
Lucca <luccag.barreto@gmail.com>
2022-05-27 01:26:26 -0300

    guia quase finalizada

--- Front-End/2rpnet-ui/src/pages/guide/guide.jsx
+++ Front-End/2rpnet-ui/src/pages/guide/guide.jsx
@@ -13,20 +13,54 @@ import '../../assets/css/components/button.css'
 import '../../assets/css/components/fonts.css'
 
 export default function Guide() {
-        return (
-                <div className='general-conteiner'>
-                        <VLibras />
-                        <Navbar />
-                        <div className='conteiner-guide'>
-                                <span className='h2'>Guia Definitivo</span>
-                        </div>
-                        <iframe width="1246" height="701" className='video-youtube' src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <div className='body-conteiner'>
-                                <div className='conteiner-guide'>
-                                        <span className='h2'>Passo a passo</span>
+    return (
+        <div className='general-conteiner'>
+            <VLibras />
+            <Navbar />
+            <div className='conteiner-guide'>
+                <span className='h2'>Guia Definitivo</span>
+            </div>
+            <iframe width="1246" height="701" className='video-youtube' src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div className='body-conteiner'>
+                <div className='conteiner-guide'>
+                    <span className='h2'>Passo a passo</span>
+                    <div className='timeline'>
+                        <ul>
+                            <li>
+                                <div className='timeline-content'>
+                                    <span className='h3 body-title-guide'>What is Lorem Ipsum?</span>
+                                    <span className='p text-guide'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
                                 </div>
-                        </div>
-                        <Footer />
+                                <div className='time'>
+                                    <h4>January 2018</h4>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='timeline-content'>
+                                    <span className='h3 body-title-guide'>What is Lorem Ipsum?</span>
+                                    <span className='p text-guide'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
+                                </div>
+                                <div className='time'>
+                                    <h4>January 2018</h4>
+                                </div>
+                            </li>
+                            
+                            <li>
+                                <div className='timeline-content'>
+                                    <span className='h3 body-title-guide'>What is Lorem Ipsum?</span>
+                                    <span className='p text-guide'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span>
+                                </div>
+                                <div className='time'>
+                                    <h4>January 2018</h4>
+                                </div>
+                            </li>
+                            <div></div>
+                        </ul>
+                    </div>
+                    
                 </div>
-        )
+            </div>
+            {/* <Footer /> */}
+        </div>
+    )
 }
