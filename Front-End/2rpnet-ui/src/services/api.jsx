b97da7633fc7bc156e1b232071c7f86e15130f5e
gustrpaz@gmail.com <gustrpaz@gmail.com>
2022-06-03 10:48:35 -0300

    consumindo a VM

--- Front-End/2rpnet-ui/src/services/api.jsx
+++ Front-End/2rpnet-ui/src/services/api.jsx
@@ -1,3 +1,5 @@
- export const API = "http://localhost:5000";
+//  export const API = "http://localhost:5000";
+//  http://191.233.241.74:5000/
+ export const API = " http://191.233.241.74:5000";
 // export const API = "http://vmbots4rpa.brazilsouth.cloudapp.azure.com:5000";
 
