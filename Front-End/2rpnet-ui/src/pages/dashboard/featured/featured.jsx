e9dce77d176e430b0bfa43a4eb4344650fa77c0f
PedroLCamara <plcrlschool@gmail.com>
2022-06-08 23:47:13 -0300

    DashboardOkay

--- Front-End/2rpnet-ui/src/pages/dashboard/featured/featured.jsx
+++ Front-End/2rpnet-ui/src/pages/dashboard/featured/featured.jsx
@@ -4,23 +4,29 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import { useEffect } from "react";
 
-const Featured = () => {
+const Featured = ({value}) => {
+        useEffect(() => {
+                if(value === undefined){
+                        value = 0;
+                }
+        }, [])
         return (
                 <div className="featured">
                         <div className="top">
-                                <h1 className="h5 title">Sucesso de Assistentes(%)</h1>
+                                <h1 className="h5 title">Sucesso do Assistente(%)</h1>
                                 <MoreVertIcon fontSize="small" />
                         </div>
                         <div className="bottom">
                                 <div className="featuredChart">
-                                        <CircularProgressbar value={89} text={"89%"} strokeWidth={5} />
+                                        <CircularProgressbar value={value} text={value + "%"} strokeWidth={5} />
                                 </div>
-                                <p className="h4 title">Taxa de Sucesso de Assistentes</p>
+                                {/* <p className="h4 title">Taxa de Sucesso de Assistentes</p>
                                 <p className="p amount"></p>
-                                <p className="desc">
-                                </p>
-                                <div className="summary">
+                                <p className="desc"> */}
+                                {/* </p> */}
+                                {/* <div className="summary">
                                         <div className="item">
                                                 <div className="p itemTitle">Perda</div>
                                                 <div className="itemResult negative">
@@ -42,7 +48,7 @@ const Featured = () => {
                                                         <div className="p resultAmount">20.2%</div>
                                                 </div>
                                         </div>
-                                </div>
+                                </div> */}
                         </div>
                 </div>
         );
